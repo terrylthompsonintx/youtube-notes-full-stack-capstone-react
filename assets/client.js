fb73b9100a5a21c0b8c7f4e631fb843fbec21342
Terry Thompson <terrylthompsonintx@gmail.com>
2018-01-31 14:25:05 -0600

    Fixed another bug.

--- assets/client.js
+++ assets/client.js
@@ -246,8 +246,7 @@ $(document).on('click', '.selectButton', function (event, selectedTitle, selecte
             method: 'GET',
             dataType: 'json',
             contentType: 'application/json',
-            url: '/checkyounote/'
-            selectedVidId,
+            url: '/checkyounote/' + selectedVidId,
         })
         .done(function (result) {
             console.log(result);
